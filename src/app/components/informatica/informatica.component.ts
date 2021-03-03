40a1bd53405c8a54fddca8c2a2a2c3c3b066f6de
Nirmalie <wnirmalie@yahoo.com>
2021-03-03 18:59:46 +0100

    new App

---
+++ src/app/components/informatica/informatica.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-informatica',
+  templateUrl: './informatica.component.html',
+  styleUrls: ['./informatica.component.css']
+})
+export class InformaticaComponent implements OnInit {
+  testo1: string ="testo di prova"
+  testo2: number = 2;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.testo1 = "lorum ipsum";
+    //this.classeDemo();
+  }
+  classeDemo(){
+    this.testo2 = 3
+  }
+
+}
